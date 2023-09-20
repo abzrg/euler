0437f9f2f5f2887c83ff91f4389ebcb8b3cb98f4
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2023-09-21 00:16:21 +0330

    Add a template file

---
+++ src/template.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
